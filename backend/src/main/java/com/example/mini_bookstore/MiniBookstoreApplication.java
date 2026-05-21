6c342db55c3a7ddfdc3fe4fe30d7446fe34cc8be
tus <===>
2026-05-22 04:22:17 +0700

    config: java 1.8 instead of java 21

---
+++ backend/src/main/java/com/example/mini_bookstore/MiniBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBookstoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/mini_bookstore/MiniBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
